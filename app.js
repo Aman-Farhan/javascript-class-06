128b9375fb5c0cc5b0efe363a5b627fb5ac42fbf
Aman <amanfarhan2008@gmail.com>
2025-07-08 01:03:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function changeText(){
+    let num1 = +prompt("Enter your 1st nuber");
+    let num2 = +prompt("Enter your 1st nuber");
+    let sum = num1 + num2 ;
+    let p = document.getElementById("changepara");
+    let show = p.innerHTML 
+};
